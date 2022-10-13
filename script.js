b1b9e9a3a5a8d66ac7db86e992ccc06230207f3f
AppleEve <etandruszkiewicz@gmail.com>
2022-10-13 12:05:46 +0300

    Creating the final score option

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
 let options = ["rock" , "paper" , "scissors"];
+let win = "You win 🥳!"
+let loose = "You loose 😞! ";
+let tie = "No winner 😒"
 
 
 function computerPlay(){
     let randomNumber = Math.floor(Math.random()*3);
-   return options[randomNumber];
+    return options[randomNumber];
 }
 
-function playRound(playerSelection, computerSelection, scoreNumber){
+function playRound(playerSelection, computerSelection){
     console.log("Your selection: " + playerSelection);
     console.log("Computer selection: " + computerSelection);
-    let winnerMessage = ("You win 🥳! " + playerSelection + " beat(s) " + computerSelection);
-    let looserMessage = ("You loose 😞! " + computerSelection + " beat(s) " + playerSelection);
+    let winnerMessage = (playerSelection + " beat(s) " + computerSelection);
+    let looserMessage = (computerSelection + " beat(s) " + playerSelection);
     let tieMessage = ("It's a tie 👔! You both played " + playerSelection);
     let errorMessage = ("Please select again between rock, paper and scissors.");
     if (playerSelection == "rock" && computerSelection == "scissors" ||
     playerSelection == "paper" && computerSelection == "rock" ||
     playerSelection == "scissors" && computerSelection == "paper"){
-        scoreNumber = scoreNumber + 1;
-        return(winnerMessage);
+        console.log(winnerMessage);
+        return win;
     } else if (computerSelection == "rock" && playerSelection == "scissors" ||
     computerSelection == "paper" && playerSelection == "rock" ||
     computerSelection == "scissors" && playerSelection == "paper"){
-        scoreNumber = scoreNumber - 1;
-        return(looserMessage);
+        console.log(looserMessage);
+        return loose;
     } else if (playerSelection === computerSelection){
-        return(tieMessage);
+        console.log(tieMessage);
+        return tie;
     } else {
-        return(errorMessage);
+        console.log(errorMessage);
+        return tie;
     }
 }
 
 function game(){
-    let scoreNumber = 0;
+    playerScore= 0;
+    computerScore = 0;
     for (let i=0; i<5; i++){
         let playerSelection = prompt("🪨, 🧻 or ✂️?").toLocaleLowerCase();
         let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection, scoreNumber));
-        console.log(scoreNumber);
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        if (roundResult === win){
+            playerScore++;
+        } else if (roundResult === loose){
+            computerScore++
+        } 
+    }
+    if (playerScore > computerScore){
+        console.log("CONGRATULATIONS! YOU WON THE GAME 💥")
+    } else if (computerScore > playerScore){
+        console.log("HAHA! YOU LOST THE GAME! 🙀")
+    } else {
+        console.log("UPS! IT'S A TIE! 🤷")
     }
 }
-
 
 game();
